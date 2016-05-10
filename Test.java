2a8a6638b1a698061840c21636363c26bb1fd006
Nk185 <TheNik185@yandex.ua>
2016-05-10 04:21:32 +0300

    Transport interface realization

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test{
 
     public static void Main() {
 
-        int someVar = 5;
+        int someVar  = 5;
         int someVar2 = 3;
 
         someVar = someVar - (someVar * 2 / someVar);
@@ -15,6 +15,26 @@ public class Test{
 
     public void InitCity() {
 
+    }
+}
+
+
+public class Transport implements ITransport{
+
+    private final String _transportType;
+
+    private int _currentPosX;
+    private int _currentPosY;
+
+    public void Transport(String transportType){
+
+        this._transportType = transportType;
+    }
+
+    public boolean Move(int toX, int toY, double speed)){
+
+        this._currentPosX = toX;
+        this._currentPosY = toY;
     }
 }
 
