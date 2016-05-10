2b0f37e41a9962496fae7a6a6a2626127a4a0f24
Nk185 <TheNik185@yandex.ua>
2016-05-10 04:17:53 +0300

    Initial

---
+++ Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Test{
+
+    public static void Main(){
+
+        int someVar = 5;
+        int someVar2 = 3;
+
+        someVar = someVar - (someVar * 2 / someVar);
+    }
+}
